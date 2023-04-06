770e7119cb27e3e4f22f11b27c5c5b552df41629
sulfin <sulfin83@gmail.com>
2023-04-06 14:14:20 +0200

    generate root certificate

--- ACR/src/acrconfig.rs
+++ ACR/src/acrconfig.rs
@@ -1,4 +1,28 @@
 
-pub struct AcrConfig{
+#[derive(Debug, Clone, Default)]
+pub struct AcrConfig {
+    pub valid_time: u32,
+    pub path_len: u32,
+    pub country: String,
+    pub common_name: String,
+}
 
+impl AcrConfig {
+    pub fn new(valid_time: u32,path_len: u32 ,country: String, common_name: String) -> Self {
+        AcrConfig {
+            valid_time,
+            path_len,
+            country,
+            common_name,
+        }
+    }
+
+    pub fn default() -> Self {
+        AcrConfig {
+            valid_time: 365,
+            path_len: 3,
+            country: "FR".to_string(),
+            common_name: "Pas Un Virus Sign".to_string(),
+        }
+    }
 }
--- ACR/src/main.rs
+++ ACR/src/main.rs
@@ -1,10 +1,31 @@
-use actix_web::{get, post, Responder};
+use std::ops::Deref;
+
+use actix_web::{App, HttpServer, post, Responder, web};
+use log::{debug, LevelFilter};
+use simple_logger::SimpleLogger;
+
+use acr::acrconfig::AcrConfig;
 
 #[post("/generate-acr")]
-async fn generate_acr() -> impl Responder {
-    
+async fn generate_acr(config: web::Data<AcrConfig>) -> impl Responder {
+    let mut ret = "ACR generated";
+    acr::acr::generate_acr(config.into_inner().deref()).unwrap_or_else(|e| {
+        ret = "ACR generation failed";
+        println!("Error: {}", e);
+    });
+    ret
 }
 
-fn main() {
-    println!("Hello, world!");
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    SimpleLogger::new().with_level(LevelFilter::Info).env().init().unwrap();
+    debug!("Debug logging enabled");
+    HttpServer::new(|| {
+        App::new()
+            .app_data(web::Data::new(AcrConfig::default()))
+            .service(generate_acr)
+    })
+        .bind(("127.0.0.1", 8080))?
+        .run()
+        .await
 }
