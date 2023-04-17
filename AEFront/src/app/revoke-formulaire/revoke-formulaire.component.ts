f7aea5d620f50e9f7ef6f3bf0b86a2a75b4e11c6
Logia556 <lorenzo.angileri@isen.yncrea.fr>
2023-04-17 18:06:44 +0200

    revoke flow

--- AEFront/src/app/revoke-formulaire/revoke-formulaire.component.ts
+++ AEFront/src/app/revoke-formulaire/revoke-formulaire.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {HttpClient} from "@angular/common/http";
 
 @Component({
   selector: 'app-revoke-formulaire',
@@ -6,5 +7,31 @@ import { Component } from '@angular/core';
   styleUrls: ['./revoke-formulaire.component.scss']
 })
 export class RevokeFormulaireComponent {
+  constructor(private http: HttpClient) {
 
 }
+
+  submitForm() {
+    const inputNode: any = document.querySelector('#certid');
+    const inputNode2: any = document.querySelector('#email');
+    const inputNode3: any = document.querySelector('#otp');
+    const certid = inputNode.value;
+    const email = inputNode2.value;
+    const otp = inputNode3.value;
+    const formData = new FormData();
+    formData.append('certid', certid);
+    formData.append('email', email);
+    formData.append('otp', otp);
+    const uploadRes = this.http.post('http://192.168.16.42:8740/api/crt/revoke', formData);
+    console.log(formData)
+    uploadRes.subscribe((res: any) => {
+      console.log(res)
+
+      if (res.status=="ok")
+        console.log("revocation request sent")
+
+    });
+  }
+
+  }
+
