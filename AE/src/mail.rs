11259b987fd4bdafb5dea53d2a922790cf73bc5a
Logia556 <lorenzo.angileri@isen.yncrea.fr>
2023-04-20 12:55:30 +0200

    ptet ca marche

--- AE/src/mail.rs
+++ AE/src/mail.rs
@@ -1,28 +1,37 @@
-use actix_multipart::form::json::JsonFieldError::ContentType;
 use lettre::message::header::ContentType;
 use lettre::transport::smtp::authentication::Credentials;
 use lettre::{Message, SmtpTransport, Transport};
+use serde_json::{json, to_string, Value};
 
-async fn mail(){
-let email = Message::builder()
-.from("NoBody <nobody@domain.tld>".parse().unwrap())
-.reply_to("Yuin <yuin@domain.tld>".parse().unwrap())
-.to("Hei <hei@domain.tld>".parse().unwrap())
-.subject("Happy new year")
-.header(ContentType::TEXT_PLAIN)
-.body(String::from("Be happy!"))
-.unwrap();
 
-let creds = Credentials::new("smtp_username".to_owned(), "smtp_password".to_owned());
+async fn mail(custmail:&str, otp:&str) -> Result<(), lettre::Error> {
+    let mailcreds=parsemail();
+    let email = Message::builder()
+        .from("NoBody <nobody@domain.tld>".parse()?)
+        .to(custmail.parse()?)
+        .subject("OTP code")
+        .header(ContentType::TEXT_PLAIN)
+        .body(String::from("your OTP code is:",to_string(otp)))?;
+
+    let creds = Credentials::new(mailcreds[nom].to_owned(), mailcreds[mdp].to_owned());
 
 // Open a remote connection to gmail
-let mailer = SmtpTransport::relay("smtp.gmail.com")
-.unwrap()
-.credentials(creds)
-.build();
+    let mailer = SmtpTransport::relay("smtp.gmail.com")
+        .unwrap()
+        .credentials(creds)
+        .build();
 
 // Send the email
-match mailer.send(&email) {
-Ok(_) => println!("Email sent successfully!"),
-Err(e) => panic!("Could not send email: {:?}", e),
-}}
+    mailer.send(&email)?;
+
+    Ok(())
+}
+fn parsemail() {
+    let mailcred = {
+        // Load the first file into a string.
+        let text = std::fs::read_to_string("./mailcred.json").unwrap();
+
+        // Parse the string into a dynamically-typed JSON structure.
+        serde_json::from_str::<Value>(&text).unwrap()
+    };
+}
