1804b8dd051d3dfe217d1883d6fe60a1f1096bb8
Logia556 <lorenzo.angileri@isen.yncrea.fr>
2023-04-26 19:48:07 +0200

    problèmes dans le main avec les fonctions csr, mais potentiellement ok.

--- AE/src/mail.rs
+++ AE/src/mail.rs
@@ -1,37 +1,36 @@
-use actix_multipart::form::json::JsonFieldError::ContentType;
 use lettre::message::header::ContentType;
 use lettre::transport::smtp::authentication::Credentials;
 use lettre::{Message, SmtpTransport, Transport};
+use serde_json::{to_string, Value};
 
 
-async fn mail(custmail:&str, otp:&str) -> Result<(), lettre::Error> {
-    let mailcreds=parsemail();
+async fn mail(custmail:&str, otp:&str) {
+    let mailcred = {
+        // Load the first file into a string.
+        let text = std::fs::read_to_string("../mailcred.json").unwrap();
+
+        // Parse the string into a dynamically-typed JSON structure.
+        serde_json::from_str::<Value>(&text).unwrap()
+    };
     let email = Message::builder()
-        .from("NoBody <nobody@domain.tld>".parse()?)
-        .to(custmail.parse()?)
+        .from("NoBody <nobody@domain.tld>".parse().unwrap())
+        .to(custmail.parse().unwrap())
         .subject("OTP code")
         .header(ContentType::TEXT_PLAIN)
-        .body(String::from("your OTP code is:",to_string(otp)))?;
+        .body(String::from(otp)).unwrap();
+    //get credentials from mailcred.json
+    let creds = Credentials::new(
+        mailcred["username"].as_str().unwrap().to_string(),
+        mailcred["password"].as_str().unwrap().to_string(),
+    );
 
-    let creds = Credentials::new(mailcreds[nom].to_owned(), mailcreds[mdp].to_owned());
 
-// Open a remote connection to gmail
+    // Open a remote connection to gmail
     let mailer = SmtpTransport::relay("smtp.gmail.com")
         .unwrap()
         .credentials(creds)
         .build();
 
 // Send the email
-    mailer.send(&email)?;
-
-    Ok(())
-}
-fn parsemail() {
-    let mailcred = {
-        // Load the first file into a string.
-        let text = std::fs::read_to_string("./mailcred.json").unwrap();
-
-        // Parse the string into a dynamically-typed JSON structure.
-        serde_json::from_str::<Value>(&text).unwrap()
-    };
+    mailer.send(&email).unwrap();
 }
